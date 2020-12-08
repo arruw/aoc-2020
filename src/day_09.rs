b2c58ba257596cb22c98c1abf597728704cb585b
Matjaž Mav <matjaz.mav@gmail.com>
2020-12-08 13:47:07 +0100

    day9

---
+++ src/day_09.rs
@@ -0,0 +1,54 @@
+const DAY: u32 = 9;
+
+type Input = todo!();
+type Output = todo!();
+
+fn input_transformer(input: &str) -> Input {
+    todo!()
+}
+
+fn solve_part1(input: &Input) -> Output {
+    todo!()
+}
+
+fn solve_part2(input: &Input) -> Output {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{input_transformer, solve_part1, solve_part2, DAY};
+    use crate::utils::*;
+
+    const SAMPLE: &str = todo!();
+
+    #[test]
+    fn test_part1_sample() {
+        let solution = solve_part1(&input_transformer(SAMPLE));
+
+        assert_eq!(0, solution);
+    }
+
+    #[test]
+    fn test_part1_puzzle() {
+        let input = read_input(2020, DAY).unwrap();
+        let solution = solve_part1(&input_transformer(&input));
+
+        assert_eq!(0, solution);
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        let solution = solve_part2(&input_transformer(SAMPLE));
+
+        assert_eq!(0, solution);
+    }
+
+    #[test]
+    fn test_part2_puzzle() {
+        let input = read_input(2020, DAY).unwrap();
+        let solution = solve_part2(&input_transformer(&input));
+
+        assert_eq!(0, solution);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,4 @@ pub mod day_05;
 pub mod day_06;
 pub mod day_07;
 pub mod day_08;
+pub mod day_09;
