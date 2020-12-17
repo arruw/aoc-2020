3ecdbbc72908a0c6ec8adb9d0517428d11f4a537
Matjaž Mav <matjaz.mav@gmail.com>
2020-12-17 21:51:13 +0100

    day17 cleanup

--- src/day_17.rs
+++ src/day_17.rs
@@ -7,92 +7,34 @@ const DAY: u32 = 17;
 #[derive(Debug, Hash, Clone, Eq, PartialEq)]
 struct Coordinate(isize, isize, isize, isize);
 
-impl Coordinate {
-    const NEIGHBOR_DELTAS: [(isize, isize, isize, isize); 80] = [
-        (-1, -1, -1, -1),
-        (-1, -1, -1, 0),
-        (-1, -1, -1, 1),
-        (-1, -1, 0, -1),
-        (-1, -1, 0, 0),
-        (-1, -1, 0, 1),
-        (-1, -1, 1, -1),
-        (-1, -1, 1, 0),
-        (-1, -1, 1, 1),
-        (-1, 0, -1, -1),
-        (-1, 0, -1, 0),
-        (-1, 0, -1, 1),
-        (-1, 0, 0, -1),
-        (-1, 0, 0, 0),
-        (-1, 0, 0, 1),
-        (-1, 0, 1, -1),
-        (-1, 0, 1, 0),
-        (-1, 0, 1, 1),
-        (-1, 1, -1, -1),
-        (-1, 1, -1, 0),
-        (-1, 1, -1, 1),
-        (-1, 1, 0, -1),
-        (-1, 1, 0, 0),
-        (-1, 1, 0, 1),
-        (-1, 1, 1, -1),
-        (-1, 1, 1, 0),
-        (-1, 1, 1, 1),
-        (0, -1, -1, -1),
-        (0, -1, -1, 0),
-        (0, -1, -1, 1),
-        (0, -1, 0, -1),
-        (0, -1, 0, 0),
-        (0, -1, 0, 1),
-        (0, -1, 1, -1),
-        (0, -1, 1, 0),
-        (0, -1, 1, 1),
-        (0, 0, -1, -1),
-        (0, 0, -1, 0),
-        (0, 0, -1, 1),
-        (0, 0, 0, -1),
-        (0, 0, 0, 1),
-        (0, 0, 1, -1),
-        (0, 0, 1, 0),
-        (0, 0, 1, 1),
-        (0, 1, -1, -1),
-        (0, 1, -1, 0),
-        (0, 1, -1, 1),
-        (0, 1, 0, -1),
-        (0, 1, 0, 0),
-        (0, 1, 0, 1),
-        (0, 1, 1, -1),
-        (0, 1, 1, 0),
-        (0, 1, 1, 1),
-        (1, -1, -1, -1),
-        (1, -1, -1, 0),
-        (1, -1, -1, 1),
-        (1, -1, 0, -1),
-        (1, -1, 0, 0),
-        (1, -1, 0, 1),
-        (1, -1, 1, -1),
-        (1, -1, 1, 0),
-        (1, -1, 1, 1),
-        (1, 0, -1, -1),
-        (1, 0, -1, 0),
-        (1, 0, -1, 1),
-        (1, 0, 0, -1),
-        (1, 0, 0, 0),
-        (1, 0, 0, 1),
-        (1, 0, 1, -1),
-        (1, 0, 1, 0),
-        (1, 0, 1, 1),
-        (1, 1, -1, -1),
-        (1, 1, -1, 0),
-        (1, 1, -1, 1),
-        (1, 1, 0, -1),
-        (1, 1, 0, 0),
-        (1, 1, 0, 1),
-        (1, 1, 1, -1),
-        (1, 1, 1, 0),
-        (1, 1, 1, 1),
-    ];
-    fn neighbors(&self) -> impl Iterator<Item = Coordinate> + '_ {
-        Coordinate::NEIGHBOR_DELTAS
+enum Dimension {
+    Three,
+    Four,
+}
+
+lazy_static! {
+    static ref NEIGHBOR_DELTAS: Vec<(isize, isize, isize, isize)> =
+        [-1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 1]
             .iter()
+            .permutations(4)
+            .unique()
+            .map(|p| p
+                .into_iter()
+                .copied()
+                .collect_tuple::<(isize, isize, isize, isize)>()
+                .unwrap())
+            .filter(|p| *p != (0, 0, 0, 0))
+            .collect();
+}
+
+impl Coordinate {
+    fn neighbors<'a>(&'a self, dim: &'a Dimension) -> impl Iterator<Item = Coordinate> + '_ {
+        NEIGHBOR_DELTAS
+            .iter()
+            .filter(move |d| match dim {
+                Dimension::Three => d.3 == 0,
+                _ => true,
+            })
             .map(move |(dx, dy, dz, dw)| {
                 Coordinate(self.0 + dx, self.1 + dy, self.2 + dz, self.3 + dw)
             })
@@ -117,56 +59,17 @@ fn input_transformer(input: &str) -> Input {
 }
 
 fn solve_part1(input: Input) -> Output {
-    let mut active: HashSet<Coordinate> = input;
-
-    (0..6).for_each(|i| {
-        let snapshot = active.to_owned();
-
-        // Rule #1
-        snapshot
-            .iter()
-            .filter(|c| {
-                let count = c
-                    .neighbors()
-                    .filter(|c| c.3 == 0)
-                    .collect::<HashSet<_>>()
-                    .intersection(&snapshot)
-                    .count();
-
-                !(2 == count || 3 == count)
-            })
-            .for_each(|c| {
-                active.remove(c);
-            });
-
-        // Rule #2
-        snapshot
-            .iter()
-            .flat_map(|c| c.neighbors().filter(|c| c.3 == 0))
-            .filter(|c| !snapshot.contains(c))
-            .filter(|c| {
-                let count = c
-                    .neighbors()
-                    .filter(|c| c.3 == 0)
-                    .collect::<HashSet<_>>()
-                    .intersection(&snapshot)
-                    .count();
-
-                count == 3
-            })
-            .unique()
-            .for_each(|c| {
-                active.insert(c);
-            });
-    });
-
-    active.len()
+    solve_space(input, Dimension::Three, 6)
 }
 
 fn solve_part2(input: Input) -> Output {
+    solve_space(input, Dimension::Four, 6)
+}
+
+fn solve_space(input: Input, dim: Dimension, cycles: u32) -> Output {
     let mut active: HashSet<Coordinate> = input;
 
-    (0..6).for_each(|i| {
+    (0..cycles).for_each(|i| {
         let snapshot = active.to_owned();
 
         // Rule #1
@@ -174,7 +77,7 @@ fn solve_part2(input: Input) -> Output {
             .iter()
             .filter(|c| {
                 let count = c
-                    .neighbors()
+                    .neighbors(&dim)
                     .collect::<HashSet<_>>()
                     .intersection(&snapshot)
                     .count();
@@ -188,11 +91,11 @@ fn solve_part2(input: Input) -> Output {
         // Rule #2
         snapshot
             .iter()
-            .flat_map(|c| c.neighbors())
+            .flat_map(|c| c.neighbors(&dim))
             .filter(|c| !snapshot.contains(c))
             .filter(|c| {
                 let count = c
-                    .neighbors()
+                    .neighbors(&dim)
                     .collect::<HashSet<_>>()
                     .intersection(&snapshot)
                     .count();
@@ -233,6 +136,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "too long"]
     fn test_part2_sample() {
         let solution = solve_part2(input_transformer(SAMPLE));
 
@@ -240,6 +144,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore = "too long"]
     fn test_part2_puzzle() {
         let input = read_input(2020, DAY).unwrap();
         let solution = solve_part2(input_transformer(&input));
